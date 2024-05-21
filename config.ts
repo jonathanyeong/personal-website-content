55a4652a553c2b49e176756a95fd9a02d392b81d
Jonathan Yeong <jonathanyeong@gmail.com>
2024-05-21 18:38:18 -0400

    Move content to a subdirectory

---
+++ config.ts
@@ -0,0 +1,28 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z
+			.string()
+			.or(z.date())
+			.transform((val) => new Date(val)),
+		updatedDate: z
+			.string()
+			.or(z.date())
+			.optional()
+			.transform((str) => (str ? new Date(str) : undefined)),
+		heroImage: z.string().optional(),
+		draft: z.boolean().default(false),
+		featured: z.boolean(),
+		topics: z
+			.array(z.string())
+			.optional()
+			.default([])
+	}),
+});
+
+export const collections = { blog };
